310aea616fe7921788e22cff1dc21af8b3adea96
Brendan Hall <bhall@pcd-0c7p3y2-lt.eduroam.ucsf.edu>
2021-10-13 09:57:42 -0700

    idk

--- tmalignents.sh
+++ tmalignents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES='./pdb_structures*'
+FILES='./pdb_structures/*'
 for f1 in $FILES
 do
 	for f2 in $FILES
@@ -7,7 +7,7 @@ do
 	do
 		echo "Processing $f1 file..."
 		tmalign $f1 $f2 > out.txt
-		awk '/TM-score=/'  out.txt | awk '{print $2}' > outer.txt 
+		awk '/TM-score=/'  out.txt | awk '{print $2}' > outer.txt
 		awk -F"#" '{print $1, $0}' outer.txt  | sort -nr | head -1 > max.txt
 		cat max.txt > final_val.txt
 	done
