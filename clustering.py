76cbd159896ad73101b96a4d34474a6ce2f29db3
Brendan <brendan.hall@ucsf.edu>
2021-10-13 15:31:08 -0700

    temp in case i fuck this up

--- clustering.py
+++ clustering.py
@@ -1,24 +1,20 @@
-import scipy.spatial.distance as ssd
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
-from numpy import genfromtxt
 import pandas as pd
 import numpy as np
 import sys
 import subprocess
 
-def hierarchicalCluster(distanceMat, method):
-    hierarchy = linkage(distanceMat,method)
+def hierarchicalCluster(simMat, method):
+    hierarchy = linkage(simMat,method)
     dendrogram(hierarchy,orientation="left",labels=distanceMat.columns)
     plt.show()
 
-
 def main():
     method = sys.argv[-1]
-    subprocess.call("./tmalignents.sh")
-    m = pd.read_csv('final_val.csv')
-    print(m)
-    hierarchicalCluster(m,method)
+    subprocess.call(["./tmalignents.sh","./pdb_structures"])
+    simMat = pd.read_csv('final_val.csv')
+    hierarchicalCluster(simMat,method)
 
 if __name__ == '__main__':
     main()
--- tmalignents.sh
+++ tmalignents.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-FILES='./pdb_structures/*'
-ls -1 './pdb_structures' | sed -e 's/\.pdb$//' | xargs | sed -e 's/ /,/g' > final_val.csv
-for f1 in $FILES
+PDBFILES="$1/*"
+ls -1 "$1" | sed -e 's/\.pdb$//' | xargs | sed -e 's/ /,/g' > simMat.csv
+for f1 in $PDBFILES
 do
 	line=""
-	for f2 in $FILES
+	for f2 in $PDBFILES
 	do
 		tmalign $f1 $f2 > out.txt
 		awk '/TM-score=/'  out.txt | awk '{print $2}' > outer.txt
 		MAX=`awk -F"#" '{print $0}' outer.txt | sort -nr | head -1`
 		line="$line${MAX},"
 	done
-	printf "${line%?}\n" >> final_val.csv
+	printf "${line%?}\n" >> simMat.csv
 done
