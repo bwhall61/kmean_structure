250d204b635a8a09f06af9530961bb07bb99f544
Brendan <brendan.hall@ucsf.edu>
2021-10-13 16:24:16 -0700

    Command line args for custom PDB location and hierarhcial method

--- clustering.py
+++ clustering.py
@@ -1,19 +1,44 @@
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt
-import pandas as pd
-import numpy as np
+from getopt import getopt
 import sys
+import pandas as pd
 import subprocess
 
 def hierarchicalCluster(simMat, method):
     hierarchy = linkage(simMat,method)
-    dendrogram(hierarchy,orientation="left",labels=distanceMat.columns)
+    dendrogram(hierarchy,orientation="left",labels=simMat.columns)
     plt.show()
 
+def genSimilarityMatrix(pdbFiles):
+    subprocess.call(["./tmalignments.sh",pdbFiles])
+    simMat = pd.read_csv('simMat.csv')
+    return simMat
+
+def usage():
+    print('usage: clustering.py [-i pdbFiles_folder] [-m clustering_method [-h]')
+
 def main():
-    method = sys.argv[-1]
-    subprocess.call(["./tmalignents.sh","./pdb_structures"])
-    simMat = pd.read_csv('final_val.csv')
+    try:
+        opts, args = getopt(sys.argv[1:],"imh",['input','method','help'])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
+
+    method = "ward"
+    pdbFiles = "./pdb_structures"
+
+    for opt, arg in opts:
+        if opt in ["-i","--input"]:
+            pdbFiles = arg
+        elif opt in ['-m',"--method"]:
+            method = arg
+        else:
+            usage()
+            sys.exit(2)
+
+    simMat = genSimilarityMatrix(pdbFiles)
     hierarchicalCluster(simMat,method)
 
 if __name__ == '__main__':
