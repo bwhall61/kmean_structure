247c20af7858d9f9064f279e5d83b1786a7e4ac7
Brendan <brendan.hall@ucsf.edu>
2021-10-14 10:34:57 -0700

    random

--- getRandomStructures.sh
+++ getRandomStructures.sh
@@ -2,15 +2,12 @@
 mkdir -p ./random_pdb_structures
 cd ./random_pdb_structures
 
-numStructures=50
-numStructures=$1
-
-curl -l http://www.rcsb.org/pdb/files/
+numStructures=24
+#numStructures=$1
 
 while [ $(ls | wc -l) -lt $numStructures ]
 do
   ID=""
-
   # Generates a random ID to attempt to download from PDB
   ID=$ID$(cat /dev/urandom | LC_CTYPE=C tr -dc '1-9'| fold -w ${1:-1} | head -n 1)
   ID=$ID$(cat /dev/urandom | LC_CTYPE=C tr -dc 'A-Z0-9'| fold -w ${1:-3} | head -n 1)
