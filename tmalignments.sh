fe11a0a93511f01e5db12bd7c376406292bdfdec
Brendan <brendan.hall@ucsf.edu>
2021-10-14 13:40:20 -0700

    Updated comment in tmalginments (bash didnt like ''')

--- tmalignments.sh
+++ tmalignments.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-'''
+<< 'COMMENT'
 This script computes the tmalign score for all pairs of proteins contained in a directory (of type PDB).
 The input is the folder of PDB files.
 The output is a similarity matrix of filetype csv.
 
 References:
 Y. Zhang, J. Skolnick, TM-align: A protein structure alignment algorithm based on TM-score, Nucleic Acids Research, 33: 2302-2309
-'''
+COMMENT
 
 PDBFILES="$1/*"
 ls -1 "$1" | sed -e 's/\.pdb$//' | xargs | sed -e 's/ /,/g' > simMat.csv
