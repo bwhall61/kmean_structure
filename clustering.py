b214e1315b023e1dfab40a7d543948c62e07a3c3
Brendan <brendan.hall@ucsf.edu>
2021-10-13 22:08:31 -0700

    Documented

--- clustering.py
+++ clustering.py
@@ -6,15 +6,22 @@ import sys
 import pandas as pd
 import subprocess
 
+# Input: Similarity matrix, hierarchical clustering method to use. See Readme for options
+# Output: Displays a dendrogram of the clusters
 def hierarchicalCluster(simMat, method):
     hierarchy = linkage(simMat,method)
     dendrogram(hierarchy,orientation="left",labels=simMat.columns)
     plt.show()
 
+# Input: Similarity matrix, number of clusters to use
+# Output:
 def spectralCluster(simMat,nClusters):
     clustering = SpectralClustering(n_clusters=nClusters,affinity='precomputed').fit(simMat)
     print(clustering.labels_)
 
+# Input: Path to the folder containing PDB files to compare
+# Output: Simialarity matrix with pariwise TM-scores from TMalign
+# Runs the bash script tmalignments.sh which saves the simialrity matrix as simMat.csv
 def genSimilarityMatrix(pdbFiles):
     subprocess.call(["./tmalignments.sh",pdbFiles])
     simMat = pd.read_csv('simMat.csv')
@@ -23,9 +30,12 @@ def genSimilarityMatrix(pdbFiles):
 def usage():
     print('usage: clustering.py [-i pdbFiles_folder] [-m clustering_method] [-n nClusters][-h]')
 
+# Command line arguments: -i Folder containing all PDB files, -m method*, -n number of clusters
+# Default: ./pdb_structures, ward hierarchical clustering, 2 clusters
+# *spectral for spectral clustering, otherwise the algorithm for hierarchical clustering
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"i:m:h:n:",["input","method","help","nclusters"])
+        opts, args = getopt.getopt(sys.argv[1:],"i:m:n:h",["input","method","nclusters","help"])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
