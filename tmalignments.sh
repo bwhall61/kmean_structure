d87872237a0dc5335ee32dbd54af05d16977bbd0
ckuhn <catherinekuhn17@gmail.com>
2021-10-14 12:00:28 -0700

    add documentation to bash script

--- tmalignments.sh
+++ tmalignments.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+"""
+This script computes the tmalign score for all pairs of proteins contained in a directory (of type PDB).
+The input is the folder of PDB files.
+The output is a similarity matrix of filetype csv.
+"""
+
 PDBFILES="$1/*"
 ls -1 "$1" | sed -e 's/\.pdb$//' | xargs | sed -e 's/ /,/g' > simMat.csv
 for f1 in $PDBFILES
